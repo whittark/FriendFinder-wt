dac1b46a818037f718108d8d26bf5d9f8058786e
Whitney Tarkington <whittark@gmail.com>
2019-10-23 19:31:47 -0700

    Updated templated data.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# FriendFinder-wt
-A social app for scientists looking for other scientists. Uses JavaScript, Handlebars, Node.js, and Express server.
+# FriendFinder for Scientific Explorers!
+Take a brief survey to find local scientists that postulate, experiment, and discover like you do. Select 'Take Survey' to begin.
+
+## About the app
+Scientific Explorer Matchup uses a Bootstrap framework, JavaScript, JQuery, Handlebars, and Express Server to locate your ideal lab partner.
---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friendMatch = [
+  {
+    name: "Dr. Frankenstein",
+    photo: "https://i.pinimg.com/originals/22/24/92/222492acb24a2c372bee174419c72acc.jpg ",
+    scores: ["2", "2", "2", "2", "2"]
+  }, {
+    name: "Albert Einstein",
+    photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/einstein-1544106382.jpg?crop=0.610xw:0.805xh;0.130xw,0.195xh&resize=480:* ",
+    scores: ["3", "3", "3", "3", "3"]
+  }, {
+    name: "Charles Darwin",
+    photo: "https://thumbs-prod.si-cdn.com/dy0zULoVdRBqBwAO2-IRr-6bRiw=/420x240/https://public-media.si-cdn.com/filer/Charles-Darwin-1880-631.jpg ",
+    scores: ["1", "1", "1", "1", "1"]
+  }, {
+    name: "Mr. Spock",
+    photo: "https://www.startrek.com/sites/default/files/styles/content_full/public/images/2019-07/8b10a9280bd46b8874af9b5cadec91d5.jpg?itok=3V9YqePf ",
+    scores: ["3", "3", "3", "2", "1"]
+  },
+
+];
+
+
+
+module.exports = friendMatch;
