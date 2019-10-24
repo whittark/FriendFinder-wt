da79a519c24c4eece51f8a796e2de3727e61ad99
Whitney Tarkington <whittark@gmail.com>
2019-10-23 21:22:50 -0700

    Updated surveyHtml and friendsJS

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,19 +2,23 @@ var friendMatch = [
   {
     name: "Dr. Frankenstein",
     photo: "https://i.pinimg.com/originals/22/24/92/222492acb24a2c372bee174419c72acc.jpg ",
-    scores: ["2", "2", "2", "2", "2"]
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"] 
   }, {
     name: "Albert Einstein",
     photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/einstein-1544106382.jpg?crop=0.610xw:0.805xh;0.130xw,0.195xh&resize=480:* ",
-    scores: ["3", "3", "3", "3", "3"]
+    scores: ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2"]
   }, {
     name: "Charles Darwin",
     photo: "https://thumbs-prod.si-cdn.com/dy0zULoVdRBqBwAO2-IRr-6bRiw=/420x240/https://public-media.si-cdn.com/filer/Charles-Darwin-1880-631.jpg ",
-    scores: ["1", "1", "1", "1", "1"]
+    scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
   }, {
     name: "Mr. Spock",
     photo: "https://www.startrek.com/sites/default/files/styles/content_full/public/images/2019-07/8b10a9280bd46b8874af9b5cadec91d5.jpg?itok=3V9YqePf ",
-    scores: ["3", "3", "3", "2", "1"]
+    scores: ["4", "4", "4", "4", "4", "4", "4", "4", "4", "4"]
+  },{
+    name: "Marie Curie",
+    photo: "https://cdn.commonlit.org/texts/images/000/001/141/medium/Marie_Curie.png?1501784823 ",
+    scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
   },
 
 ];
